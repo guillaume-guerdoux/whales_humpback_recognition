f50f429af692b1b43e32450a40e6f5a5b1724981
Guillaume <guillaume.guerdoux@hotmail.fr>
2018-02-03 19:09:14 +0100

    Finetune InceptionV3 - For the moment : low train and val accuracy

--- train.py
+++ train.py
@@ -5,9 +5,12 @@ import cv2
 from lenet import LeNet
 from keras.utils import np_utils
 from sklearn.model_selection import train_test_split
-import keras.backend as K
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.optimizers import Adam
+from keras.applications.inception_v3 import InceptionV3
 from sklearn.model_selection import train_test_split
+from keras.layers import Input
 
 # https://www.pyimagesearch.com/2017/12/11/image-classification-with-keras-and-deep-learning/#
 
@@ -38,9 +41,9 @@ def read_random_image(image_path, resize_size, train):
     img = cv2.imread(
         image_path + "/" + img_name)
     img = cv2.resize(img, (resize_size, resize_size))
-    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray_image = gray_image.astype(np.float32)
-    return gray_image, img_name
+    #gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray_image = gray_image.astype(np.float32)
+    return img, img_name
 
 
 def import_labels():
@@ -61,26 +64,33 @@ def data_generator(batch_size, dict_labels, unique_labels, rezise_size, train):
         x_train = []
         y_train = []
         for i in range(batch_size):
-            img, img_name = read_random_image(custom_train_data_dir, rezise_size, train)
+            img, img_name = read_random_image(
+                custom_train_data_dir, rezise_size, train)
             x_train.append(img)
             y_train.append(dict_labels[img_name])
         y_train = np_utils.to_categorical(y_train, len(unique_labels))
         x_train = np.array(x_train, dtype="float") / 255.0
-        x_train = x_train.reshape(batch_size, rezise_size, rezise_size, 1)
+        x_train = x_train.reshape(batch_size, rezise_size, rezise_size, 3)
         yield x_train, y_train
 
 
 if __name__ == "__main__":
     dict_labels, unique_labels = import_labels()
-    rezise_size = 228
-    model = LeNet.build(width=rezise_size, height=rezise_size,
-                        depth=1, nb_classes=len(unique_labels))
-    model.compile(loss="categorical_crossentropy", optimizer="adam",
-                  metrics=["accuracy"])
-
-    model.fit_generator(data_generator(batch_size, dict_labels, unique_labels, rezise_size, True),
+    resize_size = 224
+    input_tensor = Input(shape=(resize_size, resize_size, 3))
+    base_model = InceptionV3(input_tensor=input_tensor, weights="imagenet", include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation='relu')(x)
+    predictions = Dense(len(unique_labels), activation="softmax")(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    for layer in base_model.layers:
+        layer.trainable=False
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit_generator(data_generator(batch_size, dict_labels, unique_labels, resize_size, True),
                         samples_per_epoch=250, nb_epoch=20,
-                        validation_data=data_generator(batch_size, dict_labels, unique_labels, rezise_size, False),
+                        validation_data=data_generator(batch_size, dict_labels, unique_labels, resize_size, False),
                         validation_steps=60)
     '''model = LeNet.build(width=x_train.shape[1], height=x_train.shape[2],
                         depth=x_train.shape[3], nb_classes=y_train.shape[1])
