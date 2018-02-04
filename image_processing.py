23c9c74b2bfb4674abfc49709c9a0db04706d660
Guillaume <guillaume.guerdoux@hotmail.fr>
2018-02-04 19:22:36 +0100

    Remove unused function and add vertical flip function

--- image_processing.py
+++ image_processing.py
@@ -5,7 +5,7 @@ import os
 
 
 def random_crop(img):
-    height, width, channels = img.shape
+    height, width = img.shape
     try:
         top_crop = randint(35, int(height / 8))
         bottom_crop = height - randint(35, int(height / 8))
@@ -25,23 +25,9 @@ def blur(img, kernel_size):
     return blur_img
 
 
-def random_rotate(img):
-    negative_angle = randint(-20, -10)
-    positive_angle = randint(10, 20)
-    if abs(negative_angle) >= positive_angle:
-        angle = negative_angle
-    else:
-        angle = positive_angle
-    height = img.shape[0]
-    width = img.shape[1]
-    M = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
-    rotated_img = cv2.warpAffine(img, M, (width, height))
-    return rotated_img
-
-
 def random_rotate_zoom(img):
-    negative_angle = randint(-20, -10)
-    positive_angle = randint(10, 20)
+    negative_angle = randint(-10, -10)
+    positive_angle = randint(10, 10)
     if abs(negative_angle) >= positive_angle:
         angle = negative_angle
     else:
@@ -54,21 +40,5 @@ def random_rotate_zoom(img):
     return rotated_zoomed_img
 
 
-def random_translate(img):
-    height, width, channels = img.shape
-    vertical_translation = randint(- int(height / 25), int(height / 25))
-    horizontal_translation = randint(- int(width / 25), int(width / 25))
-    M = np.float32([[1, 0, horizontal_translation],
-                    [0, 1, vertical_translation]])
-    translated_img = cv2.warpAffine(img, M, (width, height))
-    # rotated_translated_img = random_rotate(dst)
-    return translated_img
-
-
-def jittering(img):
-    height, width, channels = img.shape
-    noise = np.random.randint(0, 35, (height, width))
-    zitter = np.zeros_like(img)
-    zitter[:, :, 1] = noise
-    jittered_img = cv2.add(img, zitter)
-    return jittered_img
+def vertical_flip(img):
+    return cv2.flip(img, 1)
